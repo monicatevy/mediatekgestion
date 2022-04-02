25092d489b410b4ef32ddfe41a0610f59d218e89
monicatevy <monicatevy.sen@pm.me>
2022-04-02 18:07:21 +0700

    ajout du projet tests unitaires

---
+++ Mediatek86Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mediatek86Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mediatek86Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcced4f9-704b-442f-a7e1-b1b6412c2a71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mediatek86Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mediatek86.controleur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediatek86.controleur.Tests
+{
+    [TestClass()]
+    public class ControleTests
+    {
+        private readonly Controle controleur = new Controle();
+        private readonly DateTime beforeDate = new DateTime(2022, 1, 1);
+        private readonly DateTime btwDate = new DateTime(2022, 6, 1);
+        private readonly DateTime afterDate = new DateTime(2022, 12, 1);
+
+        [TestMethod()]
+        public void ParutionDansAbonnementTest()
+        {
+            // Date parution valide
+            bool resultat1 = controleur.ParutionDansAbonnement(beforeDate, afterDate, btwDate);
+            Assert.AreEqual(true, resultat1, "Succès test : dateparution COMPRISE entre date abonnement et fin abonnement => TRUE");
+
+            // Date parution égale aux bornes
+            bool resultat2 = controleur.ParutionDansAbonnement(beforeDate, afterDate, beforeDate);
+            Assert.AreEqual(false, resultat2, "Succès test : dateparution EGALE à date abonnement => FALSE");
+
+            bool resultat3 = controleur.ParutionDansAbonnement(beforeDate, afterDate, afterDate);
+            Assert.AreEqual(false, resultat3, "Succès test : dateparution EGALE à date fin abonnement => FALSE");
+
+            // Date parution en dehors des bornes
+            bool resultat4 = controleur.ParutionDansAbonnement(btwDate, afterDate, beforeDate);
+            Assert.AreEqual(false, resultat4, "Succès test : dateparution AVANT date fin abonnement => FALSE");
+
+            bool resultat5 = controleur.ParutionDansAbonnement(beforeDate, btwDate, afterDate);
+            Assert.AreEqual(false, resultat5, "Succès test : dateparution APRES date fin abonnement => FALSE");
+        }
+    }
+}
